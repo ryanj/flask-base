a8bfa42d873be391219cc9cfea94881519813138
ryanj <ryan.jarvinen@gmail.com>
2014-04-26 19:19:30 -0700

    Creating a basic Flask example application

---
+++ README.md
@@ -0,0 +1,20 @@
+# A basic Flask quickstart 
+*With support for serving easy APIs and static content*
+
+To deploy a clone of this application using the [`rhc` command line tool](http://rubygems.org/gems/rhc):
+
+    rhc app create parks python-2.7 --from-code=https://github.com/ryanj/flask-static.git
+    
+Or [link to a web-based clone+deploy](https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=python-2.7&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Fflask-static.git) on [OpenShift Online](http://OpenShift.com) or on [your own OpenShift cloud](http://openshift.github.io): 
+
+    https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=python-2.7&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Fflask-static.git
+
+## Local server
+Start a local webserver by running:
+
+```bash
+python app.py
+```
+
+## License
+This code is dedicated to the public domain to the maximum extent permitted by applicable law, pursuant to CC0 (http://creativecommons.org/publicdomain/zero/1.0/)
---
+++ app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# This file may be used instead of Apache mod_wsgi to run your python
+# web application in a different framework.  A few examples are
+# provided (cherrypi, gevent), but this file may be altered to run
+# whatever framework is desired - or a completely customized service.
+#
+import imp
+import os
+import sys
+
+try:
+   virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+   python_version = "python"+str(sys.version_info[0])+"."+str(sys.version_info[1]) 
+   os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib', python_version, 'site-packages')
+   virtualenv = os.path.join(virtenv, 'bin','activate_this.py')
+   execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+   pass
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+
+#
+#  main():
+#
+if __name__ == '__main__':
+   flaskapp = imp.load_source('app', 'flaskapp.py')
+   port = flaskapp.app.config['PORT']
+   ip = flaskapp.app.config['IP']
+   app_name = flaskapp.app.config['APP_NAME']
+   host_name = flaskapp.app.config['HOST_NAME']
+
+   fwtype="wsgiref"
+   for fw in ("gevent", "cherrypy", "flask"):
+      try:
+         imp.find_module(fw)
+         fwtype = fw
+      except ImportError:
+         pass
+
+   print('Starting WSGIServer type %s on %s:%d ... ' % (fwtype, ip, port))
+   if fwtype == "gevent":
+      from gevent.pywsgi import WSGIServer
+      WSGIServer((ip, port), flaskapp.app).serve_forever()
+
+   elif fwtype == "cherrypy":
+      from cherrypy import wsgiserver
+      server = wsgiserver.CherryPyWSGIServer(
+         (ip, port), flaskapp.app, server_name=host_name)
+      server.start()
+
+   elif fwtype == "flask":
+      from flask import Flask
+      server = Flask(__name__)
+      server.wsgi_app = flaskapp.app
+      server.run(host=ip, port=port)
+
+   else:
+      from wsgiref.simple_server import make_server
+      make_server(ip, port, flaskapp.app).serve_forever()
